d05f2c5a9ae9ebdc773655aac6b5a706e485b9d3
SeokCheol <lsc991123@gmail.com>
2024-01-22 23:14:09 +0900

    feat : [011-PRODUCT-DELETE] Product & ProductItem Delete

--- order-api/src/main/java/com/example/order/controller/SellerProductController.java
+++ order-api/src/main/java/com/example/order/controller/SellerProductController.java
@@ -12,11 +12,13 @@ import com.example.order.service.ProductItemService;
 import com.example.order.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -54,5 +56,19 @@ public class SellerProductController {
         @RequestBody UpdateProductItemForm form){
         return ResponseEntity.ok(ProductItemDto.from(
             productItemService.updateProductItem(provider.getUserVo(token).getId(),form)));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteProduct(@RequestHeader(name = "X-AUTH-TOKEN")String token,
+        @RequestParam Long id){
+        productService.deleteProduct(provider.getUserVo(token).getId(),id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/item")
+    public ResponseEntity<Void> deleteProductItem(@RequestHeader(name = "X-AUTH-TOKEN")String token,
+        @RequestParam Long id){
+        productItemService.deleteProductItem(provider.getUserVo(token).getId(),id);
+        return ResponseEntity.ok().build();
     }
 }
